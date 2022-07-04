809fe26df15f2b67511b47a63eba0c4920876be0
asakza <67541862+asakza@users.noreply.github.com>
2022-07-05 00:28:30 +0400

    Update 01. Identify_even_odd.py

--- 01. Identify_even_odd.py
+++ 01. Identify_even_odd.py
@@ -1,6 +1,7 @@
 print("Good Morning Please input the value")
 entered_value = int(input('Please enter the value '))
-if entered_value == 2 :
-    print('You have entered even value')
+Chk_vle_type = entered_value%2
+if Chk_vle_type == 0 :
+    print('Entered value is Even')
 else :
     print('Entered value is odd')
